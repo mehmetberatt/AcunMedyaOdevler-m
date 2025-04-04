71379064ed364e389b4f4949db2e48ee281b17be
mehmetberatt <mehmetberattrn@gmail.com>
2025-04-04 22:19:07 +0300

    21.03odev

---
+++ odevlerım5/odevlerım5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace odevlerım5
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> arabalar = new List<Car>()
+            {
+                new Car { Marka = "Toyota", Model = "Corolla", FuelConsumption = 6.5, TotalDistance = 5000 },
+                new Car { Marka = "BMW", Model = "X5", FuelConsumption = 9.0, TotalDistance = 5000 },
+                new Car { Marka = "Mercedes", Model = "C200", FuelConsumption = 8.2, TotalDistance = 5000 }
+            };
+
+            foreach (var araba in arabalar)
+            {
+                double toplamTuketim = araba.CalculateTotalFuel();
+                Console.WriteLine($"Marka: {araba.Marka}, Model: {araba.Model}, 100 km'de Yaktığı Yakıt: {araba.FuelConsumption} litre, Toplam Yakıt Tüketimi: {toplamTuketim} litre");
+            }
+        }
+    }
+}
---
+++ odevlerım5/odevlerım5/car.cs
@@ -0,0 +1,15 @@
+﻿namespace odevlerım5
+{
+    public class Car
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public double FuelConsumption { get; set; } // 100 km'de yakıt (litre)
+        public double TotalDistance { get; set; }   // km
+
+        public double CalculateTotalFuel()
+        {
+            return (TotalDistance / 100) * FuelConsumption;
+        }
+    }
+}
